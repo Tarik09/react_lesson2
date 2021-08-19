10c48ab556e6cb2cdbc383ec2c5015fb03bb69ae
Tarik09 <nudga.t4@gmail.com>
2021-08-19 08:55:02 +0300

    commit1

---
+++ src/components/users/Users.js
@@ -0,0 +1,37 @@
+import './Users.css'
+import {useEffect, useState} from "react";
+import {getUsers} from "../../services/user.fetch.service";
+import User from "../user/User";
+
+export default function Users() {
+
+  let [users, setUsers] = useState([]);
+  let [user, setUser] = useState(null);
+
+  useEffect(() => {
+    getUsers().then(value => setUsers([...value]))
+  }, []);
+
+const choseUser = (u) => {
+        setUser({...u});
+}
+
+  return (
+    <div className={'wrap'}>
+      <div className={'users-box'}>
+        {
+          users.map(value =>
+              <User
+                  key={value.id}
+                  item={value}
+                  choseUser={choseUser}
+              />
+          )
+        }
+      </div>
+        {
+            user &&( <div className={'chosen-one'}>{JSON.stringify(user.username)}</div>)
+        }
+    </div>
+  );
+}
